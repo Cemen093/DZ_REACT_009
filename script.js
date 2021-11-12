371dbaddb3d0198583d243ef9cb1b10950b49e2e
Cemen <CemenP93@gmail.com>
2021-11-12 20:37:57 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,102 @@
+// var x;
+
+// while(true){
+//     x = prompt("Enter age");
+//     if (Number.isInteger(parseInt(x))){
+//         if(x > 17 && x < 120){
+//             alert("age is " + x + " you can buy beer");
+//         } else {
+//             alert("age is " + x + " you can't buy beer");
+//         }
+//         break;
+//     }
+//     alert("pleas input number");
+// }
+
+
+// while(true){
+//     str = prompt("Enter 1 number");
+//     if (Number.isInteger(parseInt(str))){
+//         var a = str;
+//         break;
+//     }
+//     alert("pleas input number");
+// }
+
+
+// while(true){
+//     str = prompt("Enter 2 number");
+//     if (Number.isInteger(parseInt(str))){
+//         var b = str;
+//         break;
+//     }
+//     alert("pleas input number");
+// }
+
+
+// while(true){
+//     str = prompt("Enter operator");
+//     var result;
+//     if (str == "+" || str == "-" || str == "*" || str == "/"){
+//         alert("result: " + eval(a + str + b));
+//     }else{
+//         alert("pleas input operator");
+//         continue;
+//     }
+//     break;
+// }
+
+
+// let name = prompt("Enter name");
+// let surname = prompt("Enter surname");
+// let middle_name = prompt("Enter middle name");
+// alert(name + " " + surname + " " + middle_name);
+
+//create function
+// function Name(){
+//     alert("in func Name()");
+//     return;
+// }
+// Name();
+
+// function SumOf(x , y){
+//     return x + y;
+// }
+
+// alert(SumOf(1, 2));
+
+
+// while(true){
+//     str = prompt("Enter 1 number");
+//     if (Number.isInteger(parseInt(str))){
+//         var a = str;
+//         break;
+//     }
+//     alert("pleas input number");
+// }
+
+
+// while(true){
+//     str = prompt("Enter 2 number");
+//     if (Number.isInteger(parseInt(str))){
+//         var b = str;
+//         break;
+//     }
+//     alert("pleas input number");
+// }
+
+// alert("result: " + eval("a ** b"));
+
+
+// function OnMyClick(){
+//     document.getElementById('result').value = 
+//     parseInt(document.getElementById('a').value) ** parseInt(document.getElementById('b').value);
+// }
+
+// function OnMyClick(){
+//     alert(
+//         'login='+document.getElementById('Login').value + "\n"+
+//         'password='+document.getElementById('Password').value + "\n"+ 
+//         'email='+document.getElementById('Email').value + "\n"
+//     );
+// }
